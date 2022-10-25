cbcfa093741b9c0714613c07115fa29c862c1440
ShermanKsenia <71763287+ShermanKsenia@users.noreply.github.com>
2022-10-25 22:30:54 +0300

    Add files via upload

--- processing.py
+++ processing.py
@@ -1,6 +1,7 @@
 import re
 import pymorphy2
 from pymorphy2 import MorphAnalyzer
+import sqlite3
 
 class Processing():
 
@@ -95,3 +96,128 @@ class Processing():
                             break
         return request
 
+class GetData():
+
+    def __init__(self, q, conn):
+        self.q = q
+        self.conn = conn
+        self.cur = conn.cursor()
+
+    def token_and_pos(self, dct):
+
+        query = """
+        SELECT token_id, sent_id
+        FROM tokens
+        WHERE token = ? AND pos = ?
+        """
+        self.cur.execute(query, (dct['token'], dct['pos']))
+        result = self.cur.fetchall()
+        return result
+
+    def lemma_and_pos(self, dct):
+        query = """
+        SELECT token_id, sent_id
+        FROM tokens
+        WHERE lemma = ? AND pos = ?
+        """
+        self.cur.execute(query, (dct['lemma'], dct['pos']))
+        result = self.cur.fetchall()
+        return result
+
+    def only_token(self, dct):
+        query = """
+        SELECT token_id, sent_id
+        FROM tokens
+        WHERE token = ?
+        """
+        self.cur.execute(query, (dct['token'],))
+        result = self.cur.fetchall()
+        return result
+
+    def only_lemma(self, dct):
+        query = """
+        SELECT token_id, sent_id
+        FROM tokens
+        WHERE lemma = ?
+        """
+        self.cur.execute(query, (dct['lemma'],))
+        result = self.cur.fetchall()
+        return result
+
+    def only_pos(self, dct):
+        query = """
+        SELECT token_id, sent_id
+        FROM tokens
+        WHERE pos = ?
+        """
+        self.cur.execute(query, (dct['pos'],))
+        result = self.cur.fetchall()
+        return result
+
+    def get_for_next(self, result, next):
+        sentences = []
+        if next['token']:
+            item = 'token'
+            if next['pos']:
+                item += ' = ? AND pos'
+        elif next['lemma']:
+            item = 'lemma'
+            if next['pos']:
+                item += ' = ? AND pos'
+        elif next['pos']:
+            item = 'pos'
+        t_query = f"""
+            SELECT token_id, sent_id
+            FROM tokens
+            WHERE {item} = ? AND token_id = ? AND sent_id = ?
+            """
+        for token_id, sent_id in result:
+            next_id = token_id + 1
+            if ' ' in item:
+                items = item.split(' = ? AND ')
+                self.cur.execute(t_query, (next[items[0]], next[items[1]], next_id, sent_id))
+            else:
+                self.cur.execute(t_query, (next[item], next_id, sent_id))
+                cur_sent = self.cur.fetchone()
+            if cur_sent:
+                sentences.append(cur_sent)
+        return sentences
+
+    def sort_sentences(self):
+        if self.q[0]['token'] and self.q[0]['pos']: # если в словарь токена записывать не пустые строки, а None
+            result = self.token_and_pos(self.q[0]) # id токенов и предложений
+        elif self.q[0]['lemma'] and self.q[0]['pos']:
+            result = self.lemma_and_pos(self.q[0])
+        elif self.q[0]['token'] and not self.q[0]['pos']:
+            result = self.only_token(self.q[0])
+        elif self.q[0]['lemma'] and not self.q[0]['pos']:
+            result = self.only_lemma(self.q[0])
+        else:
+            result = self.only_pos(self.q[0])
+        #print(result)
+        if result and len(self.q) > 1:
+            sentences = self.get_for_next(result, self.q[1])
+            #print(sort_sentences)
+            if len(self.q) == 3:
+                sentences = self.get_for_next(sentences, self.q[2])
+                sentences = [obj[1] for obj in sentences]
+                #print(sentences)
+                return sentences
+            else:
+                sentences = [obj[1] for obj in sentences]
+                return sentences # получаем список айдишников предложений
+        if result:
+            result = [obj[1] for obj in result]
+        return result
+
+    def get_sentences(self, ids):
+        ids = tuple(ids)
+        posts_query = f'''
+            SELECT poet, title, sent FROM sentences
+            JOIN poems_to_info ON sentences.id_sent== poems_to_info.id_sent
+            JOIN info ON poems_to_info.id_info == info.id_info
+            WHERE sentences.id_sent in {ids}
+        '''
+        self.cur.execute(posts_query)
+        result = self.cur.fetchall()
+        return result
